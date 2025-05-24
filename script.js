b795f1bf0ca43c3b6ca54ed941fbe9892f606724
Sebastián <sebasdelavega66@gmail.com>
2025-05-24 06:26:16 -0500

    Subida inicial del proyecto gestor de tareas

---
+++ script.js
@@ -0,0 +1,23 @@
+function agregarTarea() {
+    const input = document.getElementById("nuevaTarea");
+    const texto = input.value.trim();
+    if (texto === "") return;
+  
+    const li = document.createElement("li");
+    li.textContent = texto;
+  
+    li.addEventListener("click", () => {
+      li.classList.toggle("completado");
+    });
+  
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "❌";
+    btnEliminar.style.marginLeft = "10px";
+    btnEliminar.onclick = () => li.remove();
+  
+    li.appendChild(btnEliminar);
+  
+    document.getElementById("listaTareas").appendChild(li);
+    input.value = "";
+  }
+  
